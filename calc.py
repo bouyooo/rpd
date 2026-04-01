df0cedcf6004f915d22a4a72c0095a94a271ba6d
bouyooo <bouyo811@gmail.com>
2026-04-01 22:11:18 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,2 +1,26 @@
 def main():
-  
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    if divide(x, y) == None:
+        pass
+    else:
+        print("%d / %d = %0.3f" % (x, y, divide(x, y)))    
+    
+def add(a, b):
+    return a + b
+    
+    
+# TODO: divide() 함수 정의
+        
+def divide(a, b ):
+    if b != 0:
+        return a/b 
+    else : 
+        print('Error: cannot divide by zero.')
+        return None
+
+if __name__ == "__main__":
+    main()
+
